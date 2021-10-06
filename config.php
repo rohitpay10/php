d64cf81149a9bf36b8f5b4cdbd7f96bb38000a0c
rohitpay10 <91730659+rohitpay10@users.noreply.github.com>
2021-10-06 16:41:40 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,22 +1,15 @@
 <?php
 
 $global_vars = [
-'PAY_ID'=>'1144510924100302',
-'SALT'=>'10b73b1837ca411e',
+'PAY_ID'=>'merchant_payid',
+'SALT'=>'merchant_salt',
 'TXNTYPE'=>'SALE',
 'CURRENCY_CODE'=>356,
-'RETURN_URL'=>'http://localhost/projects/Pay10_newkit/response.php',
+'RETURN_URL'=>'http://localhost/your_folder/response.php',
 'PG_REQUEST_URL' =>'https://secure.pay10.com/pgui/jsp/paymentrequest'
 ];
 
 
-// $global_vars = [
-// 'PAY_ID'=>'1003510721230303',
-// 'SALT'=>'65aadf399d904df6',
-// 'TXNTYPE'=>'SALE',
-// 'CURRENCY_CODE'=>356,
-// 'RETURN_URL'=>'https://response.rs.bp.h.teamat.work/',
-// 'PG_REQUEST_URL' =>'https://secure.pay10.com/pgui/jsp/paymentrequest'
-// ];
+
 
  ?>
